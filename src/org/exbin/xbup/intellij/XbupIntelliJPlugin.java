6bdb7f4d842d3385857d73e67d2c44fe7380e2a6
hajdam <hajdam@users.sf.net>
2021-12-18 22:45:44 +0100

    Porting classes from bined plugin - 1

---
+++ src/org/exbin/xbup/intellij/XbupIntelliJPlugin.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) ExBin Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.exbin.xbup.intellij;
+
+/**
+ * Anchor class for BinEd plugin preferences root.
+ *
+ * @version 0.2.0 2021/12/18
+ * @author ExBin Project (http://exbin.org)
+ */
+public class XbupIntelliJPlugin {
+
+    public static final String PLUGIN_PREFIX = "XbupPlugin.";
+
+    private XbupIntelliJPlugin() {
+    }
+}
---
+++ src/org/exbin/xbup/intellij/XbupVirtualFile.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) ExBin Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.exbin.xbup.intellij;
+
+import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileSystem;
+import org.exbin.framework.bined.BinEdFileHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Virtual file for binary editor.
+ *
+ * @author ExBin Project (http://exbin.org)
+ * @version 0.2.0 2021/12/18
+ */
+public class XbupVirtualFile extends VirtualFile implements DumbAware {
+
+    public static final String PATH_PREFIX = "xbup://";
+
+    private final VirtualFile parentFile;
+    private String displayName;
+    private BinEdFileHandler editorFile;
+    private boolean closed = false;
+
+    public XbupVirtualFile(VirtualFile parentFile) {
+        if (parentFile.getPath().startsWith(PATH_PREFIX)) {
+            this.parentFile = LocalFileSystem.getInstance().findFileByPath(parentFile.getPath().substring(PATH_PREFIX.length()));
+        } else {
+            this.parentFile = parentFile;
+        }
+        String path = parentFile.getPath();
+        int lastIndexOf = path.lastIndexOf('/');
+        if (lastIndexOf >= 0) {
+            this.displayName = path.substring(lastIndexOf + 1);
+        } else {
+            this.displayName = "";
+        }
+    }
+
+    @Nonnull
+    public BinEdFileHandler getEditorFile() {
+        if (editorFile == null) {
+            editorFile = new BinEdFileHandler();
+        }
+
+        return editorFile;
+    }
+
+    @Nonnull
+    public JPanel getEditorPanel() {
+        return getEditorFile().getPanel();
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return parentFile.getName();
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @NotNull
+    @Override
+    public VirtualFileSystem getFileSystem() {
+        return XbupFileSystem.getInstance();
+    }
+
+    @NotNull
+    @Override
+    public String getPath() {
+        return PATH_PREFIX + parentFile.getPath();
+    }
+
+    @Override
+    public boolean isWritable() {
+        return parentFile.isWritable();
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return parentFile.isDirectory();
+    }
+
+    @Override
+    public boolean isValid() {
+        return parentFile.isValid();
+    }
+
+    @Override
+    public VirtualFile getParent() {
+        return parentFile.getParent();
+    }
+
+    @Override
+    public VirtualFile[] getChildren() {
+        return parentFile.getChildren();
+    }
+
+    @NotNull
+    @Override
+    public OutputStream getOutputStream(Object requester, long newModificationStamp, long newTimeStamp) throws IOException {
+        return parentFile.getOutputStream(requester, newModificationStamp, newTimeStamp);
+    }
+
+    @NotNull
+    @Override
+    public byte[] contentsToByteArray() throws IOException {
+        return parentFile.contentsToByteArray();
+    }
+
+    @Override
+    public long getTimeStamp() {
+        return parentFile.getTimeStamp();
+    }
+
+    @Override
+    public long getLength() {
+        return parentFile.getLength();
+    }
+
+    @Override
+    public void refresh(boolean asynchronous, boolean recursive, @Nullable Runnable postRunnable) {
+        parentFile.refresh(asynchronous, recursive, postRunnable);
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        return parentFile.getInputStream();
+    }
+
+    public long getModificationStamp() {
+        return parentFile.getModificationStamp();
+    }
+
+    @Override
+    public long getModificationCount() {
+        return parentFile.getModificationCount();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        XbupVirtualFile that = (XbupVirtualFile) o;
+        String path = getPath();
+        return path.equals(that.getPath());
+    }
+
+    @Override
+    public int hashCode() {
+        return getPath().hashCode();
+    }
+
+    public boolean isMoved() {
+        Boolean closingToReopen = getUserData(FileEditorManagerImpl.CLOSING_TO_REOPEN);
+        return closingToReopen != null && closingToReopen;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    public JComponent getPreferredFocusedComponent() {
+        return getEditorFile().getPreferredFocusedComponent();
+    }
+}
